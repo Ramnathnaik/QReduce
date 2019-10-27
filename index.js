8425d85e5d8231fe9d41be9f04482d599cfe7c5b
Ramnath <ramnathnaik447@gmail.com>
2019-10-27 23:03:30 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+$("#btn-book-appointment").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#book-appointment").offset().top},
+        'slow');
+});
+
+$("#btn-appointment-time").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#appointment-time").offset().top},
+        'slow');
+});
+
+$("#btn-complete-details").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#detailed-report").offset().top},
+        'slow');
+});
